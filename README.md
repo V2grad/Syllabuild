2218b51776d1dd7e46bd542a21c4a3f14f25c844
NeverBehave <gayhub@never.pet>
2019-02-22 16:45:43 -0500

    Serveable edition

--- README.md
+++ README.md
@@ -4,6 +4,8 @@
 
 [![Syllabuild](https://img.shields.io/badge/Syllabuild-v0.0.2-orange.svg)](https://github.com/V2grad/Syllabuild) [![RCOS](https://img.shields.io/badge/Project%20Under-RCOS-lightgreen.svg)](https://rcos.io)
 
+[![Netlify Status](https://api.netlify.com/api/v1/badges/b8def190-356c-4c2e-b650-36b3cf06bbc8/deploy-status)](https://app.netlify.com/sites/syllabuild/deploys)
+
 Auto Syllabus builder, made with [![Vuejs](https://img.shields.io/badge/vue.js-2.x-green.svg)](https://vuejs.org) and [![Bootstrap-vue](https://img.shields.io/badge/Bootstrap--Vue-2.0.0-blue.svg)](https://bootstrap-vue.js.org/).
 
 Online Demo for Master Branch: https://syb.v2grad.org
--- src/modules/data/store/actions.js
+++ src/modules/data/store/actions.js
@@ -44,14 +44,14 @@ export default {
       })
     } else {
       console.log('Note: you are using the default data set. Please check your API endpoint setting if this is not desired behavior.')
-      // Import dummy data instead.
-      // Why dummy? Google: dummy system.
-      import( /* webpackChunkName: "dummy" */ '../dummy').then((dummy) => {
-        commit('updateDataSource', 'dummy')
-        commit('updateData', dummy)
-        commit('updateTimestamp')
-        commit('updateLoading', false)
-      })
+      // // Import dummy data instead.
+      // // Why dummy? Google: dummy system.
+      // import( /* webpackChunkName: "dummy" */ '../dummy').then((dummy) => {
+      //   commit('updateDataSource', 'dummy')
+      //   commit('updateData', dummy)
+      //   commit('updateTimestamp')
+      //   commit('updateLoading', false)
+      // })
     }
   }
 }
